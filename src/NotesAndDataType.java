f31e8a874884faf6a60769ba537b0f11229a6eba
张嘉鹏 <38111184@qq.com>
2026-04-27 20:17:08 +0800

    对整数型,小数型变量进行更多类型的补充

--- src/NotesAndDataType.java
+++ src/NotesAndDataType.java
@@ -16,5 +16,27 @@ public class NotesAndDataType {
         // 布尔值(boolean): true false(是|否)(真|假)
         System.out.println(true);
 
+        /*
+        整数型
+        byte : -128 ~ 127
+        short : -32768 ~ 32767
+        *int : -2147483648 ~ 2147483647
+        long : -9223372036854775808 ~ 9223372036854775807
+        注意 :
+        1️⃣对long类型的变量赋值要在结尾加上'L'
+        2️⃣使用更大范围的变量类型将占用更大的存储空间(通常用int)
+        */
+
+        long num1 = 22000000000L;
+        System.out.println(num1);
+
+        /*
+        浮点数(小数型):
+        float : 精确到小数点后7位
+        *double : 精确到小数点后15位
+         */
+
+        double Num2 = 160.5;
+        System.out.println(Num2);
     }
 }
