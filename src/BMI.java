a9ff7964b9e4a7c8635ed3dbc0b4069780a6279b
张嘉鹏 <38111184@qq.com>
2026-04-27 21:34:10 +0800

    对BMI计算机新增判断机制,对BMI结果进行体重分析

--- src/BMI.java
+++ src/BMI.java
@@ -17,5 +17,42 @@ public class BMI {
         double BMI = weight / Math.pow(height, 2);
         BMI = Math.round(BMI * 100) / 100.0;// 保留小数点后两位
         System.out.println("您的BMI为:" + BMI);
+        /*
+            BMI计算器 + 判断机制
+            体重过轻 BMI < 18.5
+            体重正常 18.5 <=BMI < 24
+            体重过重 24 <=BMI < 27
+            轻度肥胖 27 <=BMI < 30
+            中度肥胖 30 <=BMI < 35
+            重度肥胖 BMI >= 35
+         */
+
+//        if(BMI < 18.5){
+//            System.out.println("体重过轻");
+//        } else if (BMI >= 18.5 && BMI < 24) {
+//            System.out.println("体重正常");
+//        } else if (BMI >= 24 && BMI < 27) {
+//            System.out.println("体重过重");
+//        } else if (BMI >= 27 && BMI < 30) {
+//            System.out.println("轻度肥胖");
+//        } else if (BMI >= 30 && BMI < 35) {
+//            System.out.println("中度肥胖");
+//        }else{
+//            System.out.println("重度肥胖");
+//        }
+//        上述代码仍可以进行精简,结果如下
+        if(BMI < 18.5){
+            System.out.println("体重过轻");
+        } else if (BMI < 24) {
+            System.out.println("体重正常");
+        } else if (BMI < 27) {
+            System.out.println("体重过重");
+        } else if (BMI < 30) {
+            System.out.println("轻度肥胖");
+        } else if (BMI < 35) {
+            System.out.println("中度肥胖");
+        }else{
+            System.out.println("重度肥胖");
+        }
     }
 }
