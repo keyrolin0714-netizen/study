d579548d2c87496045e03fc146f201546f6cddfb
张嘉鹏 <38111184@qq.com>
2026-04-27 19:55:58 +0800

    关于变量的类型与赋值方式

---
+++ src/Variable.java
@@ -0,0 +1,28 @@
+public class Variable {
+    public static void main(String[] args) {
+        // 字符串(String) : "小绿"
+        String name = "小绿";
+        System.out.println(name);
+        name = "小黄";
+        System.out.println(name);
+        // 字符型(char) : '王'
+        char lastname = '王';
+        System.out.println(lastname);
+        lastname = '黄';
+        System.out.println(lastname);
+        // 整数(int) : 18
+        int age = 18;
+        System.out.println(age);
+        age = 25;
+        System.out.println(age);
+        // 浮点数(double) : 160.5
+        double height = 160.5;
+        System.out.println(height);
+        //布尔型(boolean) : true false(是|否)(真|假)
+        boolean isMale = true;
+        System.out.println(isMale);
+        isMale = false;
+        System.out.println(isMale);
+
+    }
+}
