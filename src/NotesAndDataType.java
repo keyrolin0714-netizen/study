5921e5eb8639b5ea29910096871b1abb58ca77f6
张嘉鹏 <38111184@qq.com>
2026-04-27 18:54:58 +0800

    包含Java中的注释和可供打印的数据类型 其中,数值型可以用做运算

---
+++ src/NotesAndDataType.java
@@ -0,0 +1,20 @@
+// Java中的数据类型
+public class NotesAndDataType {
+    public static void main(String[] args) {
+        // 单行注释
+        /* 多行注释 */
+        /** 文本（文档）注释 */
+
+        // 字符串(String): "小绿" -- Java中' '和" "功能不同
+        System.out.println("小绿");
+        // 字符型(char): '王'
+        System.out.println('王');
+        // 整数(int): 18
+        System.out.println(18*12);
+        // 浮点数(double): 160.5
+        System.out.println(160.5);
+        // 布尔值(boolean): true false(是|否)(真|假)
+        System.out.println(true);
+
+    }
+}
