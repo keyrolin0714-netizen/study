7fd6b1037c6b8f1ddf50507242531150b3b81510
张嘉鹏 <38111184@qq.com>
2026-04-27 20:07:05 +0800

    新增在打印语句中引用变量,字符串的拼接,关于变量的命名规则

--- src/Variable.java
+++ src/Variable.java
@@ -1,28 +1,46 @@
 public class Variable {
     public static void main(String[] args) {
-        // 字符串(String) : "小绿"
-        String name = "小绿";
-        System.out.println(name);
-        name = "小黄";
-        System.out.println(name);
-        // 字符型(char) : '王'
-        char lastname = '王';
-        System.out.println(lastname);
-        lastname = '黄';
-        System.out.println(lastname);
-        // 整数(int) : 18
-        int age = 18;
-        System.out.println(age);
-        age = 25;
-        System.out.println(age);
-        // 浮点数(double) : 160.5
-        double height = 160.5;
-        System.out.println(height);
-        //布尔型(boolean) : true false(是|否)(真|假)
-        boolean isMale = true;
-        System.out.println(isMale);
-        isMale = false;
-        System.out.println(isMale);
+//        // 字符串(String) : "小绿"
+//        String name = "小绿";
+//        System.out.println(name);
+//        name = "小黄";
+//        System.out.println(name);
+//        // 字符型(char) : '王'
+//        char lastname = '王';
+//        System.out.println(lastname);
+//        lastname = '黄';
+//        System.out.println(lastname);
+//        // 整数(int) : 18
+//        int age = 18;
+//        System.out.println(age);
+//        age = 25;
+//        System.out.println(age);
+//        // 浮点数(double) : 160.5
+//        double height = 160.5;
+//        System.out.println(height);
+//        //布尔型(boolean) : true false(是|否)(真|假)
+//        boolean isMale = true;
+//        System.out.println(isMale);
+//        isMale = false;
+//        System.out.println(isMale);
+
+        //使用 '+' 可以进行字符串的拼接
+        String name = "小白";
+        System.out.println("有一个人叫" + name);
+        System.out.println(name + "今年25岁");
+        System.out.println(name + "身高179公分");
+        System.out.println("刚刚" + name + "去上厕所");
+
+        String name1_$ = "小黄";
+        /*
+        变量的命名规则
+        1️⃣只能由字母,数字,_,$来组合
+        2️⃣不能由数字开头
+        3️⃣不能使用Java中预设的关键字
+        4️⃣命名习惯:小驼峰命名法 age,myName,myFavoriteFood
+        5️⃣取有意义的名字 a b c ???
+         */
+
 
     }
 }
