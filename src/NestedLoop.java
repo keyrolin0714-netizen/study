55a5f45ae7c47f2cd5a9bb03b48037416dd2b766
张嘉鹏 <38111184@qq.com>
2026-04-29 10:30:22 +0800

    for循环嵌套与循环遍历二维数组

---
+++ src/NestedLoop.java
@@ -0,0 +1,16 @@
+// 循环嵌套
+public class NestedLoop {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                System.out.println(i + "," + j);
+//            }
+//        }
+        int [][] nums = {{1,2,3},{4,5,6},{7,8,9}};
+        for(int [] num:nums){
+            for(int num1:num){
+                System.out.println(num1);
+            }
+        }
+    }
+}
