700da9a1c9792c47878af1ed60bc5ca82cccdce4
张嘉鹏 <38111184@qq.com>
2026-04-29 10:38:35 +0800

    循环中break和continue语句的使用

---
+++ src/BreakAndContinue.java
@@ -0,0 +1,20 @@
+public class BreakAndContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5){
+                //break;
+                continue;
+            }
+            System.out.println(i);
+        }
+        int j = 0;
+        while (j < 10){
+            if (j == 5){
+                j++;
+                continue;
+            }
+            System.out.println(j);
+            j++;
+        }
+    }
+}
