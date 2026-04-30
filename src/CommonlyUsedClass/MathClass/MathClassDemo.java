51fba830747475749cb8df3c8e5ec549f7bde4a7
张嘉鹏 <38111184@qq.com>
2026-04-30 18:56:27 +0800

    常用数学类

---
+++ src/CommonlyUsedClass/MathClass/MathClassDemo.java
@@ -0,0 +1,20 @@
+package CommonlyUsedClass.MathClass;
+
+public class MathClassDemo {
+    public static void main(String[] args) {
+        // Math类的使用
+        // 绝对值
+        int abs = Math.abs(-100);
+        System.out.println(abs);
+        // 最大值
+        int max = Math.max(5,3);
+        System.out.println(max);
+        // 几次方
+        double pow = Math.pow(2,3);
+        System.out.println(pow);
+        // 开平方
+        double sqrt = Math.sqrt(2);
+        System.out.println(sqrt);
+
+    }
+}
