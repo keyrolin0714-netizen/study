e83fa7417a6239018dacff47d6858866f0d098c0
张嘉鹏 <38111184@qq.com>
2026-05-01 14:42:19 +0800

    关于类和对象的创建和基本应用

---
+++ src/Object_Oriented/DogMain.java
@@ -0,0 +1,18 @@
+package Object_Oriented;
+
+public class DogMain {
+    // main函数是程序的入口函数
+    public static void main(String[] args) {
+        // 在Java中创建一个狗狗对象
+        Dog dog = new Dog();//new相当于向内存申请一块空间
+        dog.name = "小黄";
+        dog.age = 5;
+
+        Dog dog2 = new Dog();
+        dog2.name = "小黑";
+        dog2.age = 3;
+        // 自我介绍的方法
+        dog.sayHello();
+        dog2.sayHello();
+    }
+}
