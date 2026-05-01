cdd2271f5a086d98d170c7bfb8aa1fa9a12f4d75
张嘉鹏 <38111184@qq.com>
2026-05-01 19:01:13 +0800

    关于方法的2个参数的用法

---
+++ src/Object_Oriented/MethodDemo/Calc_02.java
@@ -0,0 +1,9 @@
+package Object_Oriented.MethodDemo;
+
+// 计算类
+public class Calc_02 {
+    // 定义一个可以计算两个数字之和的方法
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
---
+++ src/Object_Oriented/MethodDemo/Calc_02Main.java
@@ -0,0 +1,11 @@
+package Object_Oriented.MethodDemo;
+
+public class Calc_02Main {
+    public static void main(String[] args) {
+        Calc_02 calc = new Calc_02();
+        int num1 = 3;
+        int num2 = 4;
+        int result = calc.add(num1, num2);
+        System.out.println(result);
+    }
+}
