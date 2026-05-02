b1a9bd38b243df07bc4afe83e3bac676f1aed10b
张嘉鹏 <38111184@qq.com>
2026-05-02 18:58:37 +0800

    关于构造方法和重载

---
+++ src/AbstractionAndEncapsulatio/DogMain.java
@@ -0,0 +1,12 @@
+package AbstractionAndEncapsulatio;
+
+public class DogMain {
+    public static void main(String[] args) {
+        // ctrl + 鼠标左键,可以在idea中转到定义
+        // 1.本质上是调用了无参构造方法
+        // 2.构造方法是可以创建对象的
+        // 3.手动写了构造,系统不再提供构造
+        Dog d = new Dog("哈士奇","二哈",3);
+        d.show();
+    }
+}
