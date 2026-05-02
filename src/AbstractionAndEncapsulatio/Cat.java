cdc2df8a1245683be8f998ea4ecb42cf7bad0780
张嘉鹏 <38111184@qq.com>
2026-05-02 19:20:45 +0800

    关于方法的重载和静态

---
+++ src/AbstractionAndEncapsulatio/Cat.java
@@ -0,0 +1,21 @@
+package AbstractionAndEncapsulatio;
+
+public class Cat {
+    static String name;
+    static int num;
+    static {
+        num++;
+        System.out.println(num);
+    }
+    // 只关注方法
+    public int test(String name,int age){
+        return 1;
+    }
+
+    public void test(int age,String name){
+
+    }
+    public static void show() {
+        System.out.println("我是static的show方法");
+    }
+}
