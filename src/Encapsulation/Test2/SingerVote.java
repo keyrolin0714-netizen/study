578996563397701e69db098a3e31c8de6bf2e449
张嘉鹏 <38111184@qq.com>
2026-05-03 16:24:52 +0800

    关于面向对象:封装的练习2

---
+++ src/Encapsulation/Test2/SingerVote.java
@@ -0,0 +1,36 @@
+package Encapsulation.Test2;
+
+// 模拟实现歌星投票过程:每个网民只允许投一次票
+// 并且当投票总数达到1000时就停止投票
+
+// 定义一个投票类
+public class SingerVote {
+    // 三个属性
+    // 投票的次数
+    static int count;
+    // 投票的最大次数
+    public static final int max_votes = 1000; // 常量
+    // 投票人
+    public String name;
+
+    public SingerVote(){
+
+    }
+
+    public SingerVote(String name){
+        this.name = name;
+    }
+    // 投票的方法
+    public void vote() {
+        // 如果投票到达了上限,提示投票结束
+        if (count < max_votes) {
+            count++;
+        }else {
+            System.out.println("投票已经结束,停止投票");
+        }
+    }
+
+    public static void show(){
+        System.out.println("目前" + count + "票");
+    }
+}
---
+++ src/Encapsulation/Test2/SingerVoteMain.java
@@ -0,0 +1,17 @@
+package Encapsulation.Test2;
+
+public class SingerVoteMain {
+    public static void main(String[] args) {
+        SingerVote vote = new SingerVote("张三");
+        SingerVote vote2 = new SingerVote("李四");
+
+
+        SingerVote.show();
+        vote.vote();
+        vote2.vote();
+        SingerVote.show();
+    }
+
+
+
+}
