30cafaea2ece0c6bbe269e9e07789d9780698553
张嘉鹏 <38111184@qq.com>
2026-05-03 21:18:31 +0800

    关于面向对象:继承的用法

---
+++ src/Inheritance/PetMain.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class PetMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = "小黄";
+        dog.run(dog.name);
+    }
+}
