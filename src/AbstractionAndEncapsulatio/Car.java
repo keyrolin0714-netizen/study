4d08755a072d9bcf3b4f11a3d0e9747df51d72d7
张嘉鹏 <38111184@qq.com>
2026-05-03 14:28:22 +0800

    关于类中的实例变量可以有多个拷贝

---
+++ src/AbstractionAndEncapsulatio/Car.java
@@ -0,0 +1,5 @@
+package AbstractionAndEncapsulatio;
+
+public class Car {
+    String brand;
+}
---
+++ src/AbstractionAndEncapsulatio/CarMain.java
@@ -0,0 +1,10 @@
+package AbstractionAndEncapsulatio;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.brand = "BMW";
+        Car car2 = new Car();
+        car2.brand = "Audi";
+    }
+}
