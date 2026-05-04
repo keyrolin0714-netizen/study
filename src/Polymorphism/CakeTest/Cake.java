83eabf6a08ae842566e99912562f08e30662e4f2
张嘉鹏 <38111184@qq.com>
2026-05-05 01:15:55 +0800

    关于面向对象:多态的练习:蛋糕店

---
+++ src/Polymorphism/CakeTest/Cake.java
@@ -0,0 +1,5 @@
+package Polymorphism.CakeTest;
+
+public abstract class Cake {
+    public abstract void getCake(String type,int size);
+}
---
+++ src/Polymorphism/CakeTest/HSRCake.java
@@ -0,0 +1,9 @@
+package Polymorphism.CakeTest;
+
+public class HSRCake extends Cake {
+
+    @Override
+    public void getCake(String type, int size) {
+        System.out.println(size+"寸"+type+"蛋糕");
+    }
+}
