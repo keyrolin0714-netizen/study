00a5aa6e4566c40f0573f2c08b80d79f821fc09a
张嘉鹏 <38111184@qq.com>
2026-05-10 11:57:16 +0800

    JDBC:工具类封装V2.0的测试

--- src/JDBC/senior/JDBCUtilTest.java
+++ src/JDBC/senior/JDBCUtilTest.java
@@ -1,9 +1,11 @@
 package JDBC.senior;
 
 import JDBC.senior.util.JDBCUtil;
+import JDBC.senior.util.JDBCUtilV2;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.util.concurrent.Callable;
 
 public class JDBCUtilTest {
     @Test
@@ -13,5 +15,25 @@ public class JDBCUtilTest {
         // CRUD
 
         JDBCUtil.release(connection);
+    }
+
+    @Test
+    public void testGetConnectionV2() {
+        /*Connection connection1 = JDBCUtil.getConnection();
+        Connection connection2 = JDBCUtil.getConnection();
+        Connection connection3 = JDBCUtil.getConnection();
+
+        System.out.println(connection1);
+        System.out.println(connection2);
+        System.out.println(connection3);*/
+
+        Connection connection1 = JDBCUtilV2.getConnection();
+        Connection connection2 = JDBCUtilV2.getConnection();
+        Connection connection3 = JDBCUtilV2.getConnection();
+
+        System.out.println(connection1);
+        System.out.println(connection2);
+        System.out.println(connection3);
+
     }
 }
