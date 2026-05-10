b47c6c5cdff34ba9087272992b763a37cc73cd35
张嘉鹏 <38111184@qq.com>
2026-05-10 13:16:12 +0800

    JDBC:DAO结合BaseDAO实现CRUD

--- src/JDBC/senior/JDBCUtilTest.java
+++ src/JDBC/senior/JDBCUtilTest.java
@@ -1,10 +1,14 @@
 package JDBC.senior;
 
+import JDBC.senior.dao.EmployeeDao;
+import JDBC.senior.dao.impl.EmployeeDaoImpl;
+import JDBC.senior.pojo.Employee;
 import JDBC.senior.util.JDBCUtil;
 import JDBC.senior.util.JDBCUtilV2;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class JDBCUtilTest {
@@ -35,5 +39,34 @@ public class JDBCUtilTest {
         System.out.println(connection2);
         System.out.println(connection3);
 
+    }
+
+    @Test
+    public void testEmployeeDao() {
+        // 1.创建DAO实现类的对象
+        EmployeeDao employeeDao = new EmployeeDaoImpl();
+
+//        // 2.调用查询所有方法
+//        List<Employee> employeeList = employeeDao.selectAll();
+//
+//        // 3.处理结果
+//        for (Employee employee : employeeList) {
+//            System.out.println("employee:"+employee);
+//
+//        }
+//        // 调用根据id查询单个员工方法
+//        Employee employee = employeeDao.selectById(1);;
+//        System.out.println("employee = " + employee);
+//        // 调用添加员工的方法
+//        Employee employee = new Employee(null,"tom",300.65,38);
+//        int insert = employeeDao.insert(employee);
+//        System.out.println("insert : " + insert);
+
+//        Employee employee = new Employee(20008,"tom",656.65,38);
+//        int update = employeeDao.update(employee);
+//        System.out.println("update :" + update);
+
+        int delete = employeeDao.delete(20008);
+        System.out.println("delete: " + delete);
     }
 }
--- src/JDBC/senior/dao/impl/EmployeeDaoImpl.java
+++ src/JDBC/senior/dao/impl/EmployeeDaoImpl.java
@@ -1,11 +1,12 @@
 package JDBC.senior.dao.impl;
 
+import JDBC.senior.dao.BaseDAO;
 import JDBC.senior.dao.EmployeeDao;
 import JDBC.senior.pojo.Employee;
 
 import java.util.List;
 
-public class EmployeeDaoImpl implements EmployeeDao {
+public class EmployeeDaoImpl extends BaseDAO implements EmployeeDao {
 
     @Override
     public List<Employee> selectAll() {
@@ -15,26 +16,51 @@ public class EmployeeDaoImpl implements EmployeeDao {
         // 4.为占位符赋值,执行SQL,接受返回结果
         // 5.处理结果
         // 6.释放资源
-        return List.of();
+        try {
+            String sql = "select emp_id empId,emp_name empName,emp_salary empSalary,emp_age empAge from t_emp";
+            return executeQuery(Employee.class,sql,null);//?
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Employee selectById(Integer empId) {
-        return null;
+        try {
+            String sql = "select emp_id empId,emp_name empName,emp_salary empSalary,emp_age empAge from t_emp where emp_id = ?";
+            return executeQueryBean(Employee.class,sql,empId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int insert(Employee employee) {
-        return 0;
+        try {
+            String sql = "INSERT INTO t_emp (emp_name,emp_salary,emp_age) VALUES (?,?,?)";
+            return executeUpdate(sql,employee.getEmpName(),employee.getEmpSalary(),employee.getEmpAge());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int update(Employee employee) {
-        return 0;
+        try {
+            String sql = "UPDATE t_emp SET emp_salary = ? WHERE emp_id = ?";
+            return executeUpdate(sql,employee.getEmpSalary(),employee.getEmpId());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int delete(Integer empId) {
-        return 0;
+        try {
+            String sql = "DELETE FROM t_emp WHERE emp_id = ?";
+            return executeUpdate(sql,empId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
